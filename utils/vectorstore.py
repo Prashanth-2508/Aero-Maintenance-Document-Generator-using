5764c51ffb12489f919a8a69a5e1783572e41670
Prashanth Kumar G V <79651138+Prashanth-2508@users.noreply.github.com>
2025-05-17 19:18:27 +0530

    vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,9 +1,28 @@
-from langchain.vectorstores import FAISS
+import faiss
+import os
+import pickle
 from sentence_transformers import SentenceTransformer
 
-def search_faiss(query: str, top_k=3):
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    vectordb = FAISS.load_local("vector_store", embedding=model)
+MODEL = SentenceTransformer("all-MiniLM-L6-v2")
+INDEX_PATH = "vector.index"
+META_PATH = "vector_meta.pkl"
 
-    docs = vectordb.similarity_search(query, k=top_k)
-    return "\n".join([doc.page_content for doc in docs])
+def build_faiss_index(text_chunks):
+    embeddings = MODEL.encode(text_chunks, show_progress_bar=True)
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(embeddings)
+
+    with open(META_PATH, "wb") as f:
+        pickle.dump(text_chunks, f)
+
+    faiss.write_index(index, INDEX_PATH)
+
+
+def search_faiss(query, top_k=5):
+    index = faiss.read_index(INDEX_PATH)
+    with open(META_PATH, "rb") as f:
+        chunks = pickle.load(f)
+
+    query_vec = MODEL.encode([query])
+    _, indices = index.search(query_vec, top_k)
+    return "\n".join([chunks[i] for i in indices[0]])
